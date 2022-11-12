8a8941c59beea04ae2b2aeaafa28407d46fe8753
unknown <cristian.undurraga.l@gmail.com>
2022-11-12 19:08:59 -0300

    se genera funcion de filtro por categoria y busqueda de producto por nombre

--- app.js
+++ app.js
@@ -25,22 +25,25 @@ app.use(function(req, res, next) {
 });
 
 //dbConnect
-const connection = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database
+const pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : process.env.host,
+    user            : process.env.user,
+    password        : process.env.password,
+    database        : process.env.database
 });
 
+
+//Captura Todas las categorias
 function getMenu() {
     return new Promise ((resolve, reject) => {
-        connection.connect((err) => {
+        pool.getConnection((err) => {
             if (err) {
                 reject(err);
             } else {
-                console.log("Connectado al servidor SQL");
+                //console.log("Capturando categorias para el menu");
                 const query = "SELECT * FROM category";
-                connection.query(query, function (err, result) {
+                pool.query(query, function(err, result){
                     if (err) {
                         reject(err);
                     } else {
@@ -52,26 +55,102 @@ function getMenu() {
     }) 
 }
 
-app.get("/", function(req, res){
+//captura todos los productos de una categoria en especifico
+function getProduct(categoryID) {
+    
+    return new Promise ((resolve, reject) => {
+        pool.getConnection((err) => {
+            if (err) {
+                reject(err);
+            } else {
+                //console.log("Capturando productos que pertenecen a la categoria seleccionada");
+                const query = "SELECT * FROM product WHERE category=" + categoryID;
+                pool.query(query, function (err, result) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                    }
+                })
+            }
+        })
+    })
+}
+
+function findProduct(productName) {
+
+    return new Promise ((resolve, reject) => {
+        pool.getConnection((err) => {
+            if (err) {
+                reject(err);
+            } else {
+                //console.log("buscando producto");
+                const query = "SELECT * FROM product WHERE name='" + productName + "'";
+                pool.query(query, function (err, result) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                    }
+                });
+            }
+        });
+    });
+}
+
+app.get("/categorias", function(req, res){
     
     //captura las categorias y las envia al front end para menu
-    getMenu().then((categorias) => {
-        const menuList = [];
-        categorias.map((categoria) => {
-            menuList.push("<li><a class=dropdown-item href=/" + categoria.name + ">" + categoria.name + "</a></li>");
+    getMenu()
+        .then((categorias) => {
+            const menuList = [];
+            categorias.map((categoria) => {
+                menuList.push("<li><a id=" + categoria.id + " class=dropdown-item href=# onclick=buscarProductos(id) >" + categoria.name + "</a></li>");
+            })
+            res.send(menuList.join(""));
         })
-        res.send(menuList.join(""));
-        //res.send(categorias);
-    }).catch((err) => {
-        console.log("Error ", err);
-    }).finally(() => {
-        connection.end();
-    });
+        .catch((err) => {
+            console.log("Error ", err);
+        })
+});
 
-})
+app.get("/categorias/:categoryid", function(req, res){
+    
+    const categoryID = req.params.categoryid;
 
+    getProduct(categoryID)
+        .then((products) => {
+            const listaProductos = [];
+            products.map((product) => {
+                listaProductos.push(
+                    "<div class=col-3><div class=card style=width: 18rem;><img src=" + product.url_image + " class=card-img-top alt=''><div class=card-body><h5 class=card-title>" + product.name + "</h5><p class=card-text>" + product.price + "</p><a href=# class='btn btn-primary'>Go somewhere</a></div></div></div>"
+                )
+            })
+            res.send(listaProductos.join(""));
+        })
+        .catch((err) => {
+            console.log("Error: ", err);
+        })
+});
 
+app.get("/buscar/:buscarProductoNombre", function(req, res){
+    const productName = req.params.buscarProductoNombre;
 
+    findProduct(productName)
+        .then((result) => {
+            const productoEncontrado = [];
+            result.map((product) => {
+                productoEncontrado.push(
+                    "<div class=col-3><div class=card style=width: 18rem;><img src=" + product.url_image + " class=card-img-top alt=''><div class=card-body><h5 class=card-title>" + product.name + "</h5><p class=card-text>" + product.price + "</p><a href=# class='btn btn-primary'>Go somewhere</a></div></div></div>"
+                )
+            })
+            
+            res.send(productoEncontrado.join(""));
+        })
+        .catch((err) => {
+            console.log("Error: ", err);
+        });
+});
 
 
 //listener
