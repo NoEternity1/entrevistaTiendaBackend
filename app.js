de5dedbd15ecf7538b3fcabf7bd4b7e466d2e69b
unknown <cristian.undurraga.l@gmail.com>
2022-11-13 17:03:11 -0300

    preparacion de archivos para deploy en heroku

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 //CORS
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "null"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://bsale-tiendaonline-entrevista.herokuapp.com/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
